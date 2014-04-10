f5f3182433b42c51016a58e34830c52069336082
yangyimeng <yangbeautydream@gmail.com>
2014-04-10 09:04:42 +0800

    prepare all the steps for powerline install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#cat ./.vimrc >> ~/.vimrc
+cat ./.vimrc > ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
